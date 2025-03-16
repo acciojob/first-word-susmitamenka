694007f68c2c51e632597c817c785b15e5642d7b
susmitamenka <susmitaprasad73@gmail.com>
2025-03-16 15:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	 // Trim leading and trailing spaces
+    str = str.trim();
+    
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
+
 }
 
 // Do not change the code below
